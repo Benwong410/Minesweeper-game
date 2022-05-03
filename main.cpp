37c2cbce6fca9d548d94cd1bf9e2c7f8f3062641
Benwong410 <101472258+Benwong410@users.noreply.github.com>
2022-05-03 19:19:40 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,24 +3,22 @@
 
 using namespace std;
 
-int side, num_mines;
+int side, num_mines; //global variable of side and number of mines
 
+//Function that decide the level of the game(board size, mines amount)
 void Level(string level)
 {
-    if (level == "Master")
-    {
+    if (level == "Master"){//Hardest level with 16*16 board and 45 mines in total
         side = 16;
         num_mines = 45;
     }
  
-    if (level == "Challenger")
-    {
+    if (level == "Challenger"){//Intermediate level with 9*9 board and 12 mines in total
         side = 9;
         num_mines = 12;
     }
  
-    if (level == "Rookie")
-    {
+    if (level == "Rookie"){//Beginner level with 5*5 board and 3 mines in total
         side = 5;
         num_mines = 3;
     }
@@ -34,7 +32,10 @@ int main()
     cout << "Please input the level of difficulty of your game: (Rookie/Challenger/Master)" << endl;
     cin >> level;
     Level (level);
-    int board [side][side];
     //cout << side<< '' << num_mines;
+    
+    int board [side][side];
+    
+    
     return 0;
 }
