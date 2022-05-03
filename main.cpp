de584a1e6dc515b331e81149f413e4a17058ef26
Benwong410 <101472258+Benwong410@users.noreply.github.com>
2022-05-03 21:36:53 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,63 @@
 #include <iostream>
 #include <string>
-#define MAXSIDE 17
+#include <cstring>
 
 using namespace std;
-
 int side, num_mines; //global variable of side and number of mines
 
-void startgame(char realboard[][MAXSIDE], char board[][MAXSIDE]){
-	srand(time (NULL));
-	for (int i=0; i<SIDE; i++){
-		for (int j=0; j<SIDE; j++){
+void startgame(char realboard[][16], char board[][16]){
+    srand(time (NULL));
+	for (int i=0; i<side; i++){
+		for (int j=0; j<side; j++){
 			board[i][j] = realboard[i][j] = '-';
 		}
 	}
 }
 
-void printboard(char board[][MAXSIDE])
+void printboard(char board[][16])
 {
-	cout << " ";
-	for (i=0; i<SIDE; i++){
+	cout << "  ";
+	
+	for (int i=0; i<side; i++){
 		cout << " " << i;
 	}
 	cout << endl;
-	for (i=0; i<SIDE; i++){
+	for (int i=0; i<side; i++){
 		cout << " " << i;
-		for (j=0; j<SIDE; j++){
+		for (int j=0; j<side; j++){
 			cout << " " << board[i][j];
 		}
 		cout << endl;
 	}
 }
+
+void Mines_implementation(int mines[][2], char minesBoard[][16])
+{
+	bool check[side*side];
+
+	//memset (mark, false, sizeof (mark));
+
+	for (int i=0; i<num_mines; )
+	{
+		int random = rand() % (side*side);
+		int x = random / side;
+		int y = random % side;
+
+		if (check[random] == false)
+		{
+			mines[i][0]= x;
+			mines[i][1] = y;
+
+			minesBoard[mines[i][0]][mines[i][1]] = '*';
+			check[random] = true;
+			i++;
+		}
+	}
+
+	return;
+}
+
+
 
 //Function that decide the level of the game(board size, mines amount)
 void Level(string level)
@@ -59,9 +87,22 @@ int main()
     cin >> level;
     Level (level);
     //cout << side<< '' << num_mines;
-    
-    int board [side][side];
-    
+
+    bool gameOver = false; 
+
+	char minesBoard[16][16], playerBoard[16][16]; //step1 : define 2 array (1 for player, 1 for the mines)
+
+    int movesremains = side * side - num_mines;
+	int mines[num_mines][2]; // step2 : define an array for mines
+
+    startgame (minesBoard, playerBoard);// step3 : create two game board
+
+    //printboard(minesBoard);
+    //printboard(playerBoard);
+    Mines_implementation (mines, minesBoard); // step4 : randomly placing mines in mines board
+
+    //printboard(minesBoard);
+    //printboard(playerBoard);
     
     return 0;
 }
