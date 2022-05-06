4161d67c2bfeeeb36cf8b69e01735ea01a2f5a2a
JaeTee211 <62334216+JaeTee211@users.noreply.github.com>
2022-05-06 12:12:11 +0800

    Create board.cpp

---
+++ board.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void startgame(char realboard[][16], char board[][16]){
+    srand(time (NULL));
+	  for (int i=0; i<side; i++){
+		  for (int j=0; j<side; j++){
+			  board[i][j] = realboard[i][j] = '-';
+		  }
+	  }
+}
+
+void printboard(char board[][16]){
+	  cout << "  ";
+	  for (int i=0; i<side; i++){
+		  cout << " " << i;
+	  }
+	  cout << endl;
+	  for (int i=0; i<side; i++){
+		  cout << " " << i;
+		  for (int j=0; j<side; j++){
+			  cout << " " << board[i][j];
+		  }
+		  cout << endl;
+	  }
+}
