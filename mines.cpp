52ae045f6d6afeb310df2382075a592eb427298c
JaeTee211 <62334216+JaeTee211@users.noreply.github.com>
2022-05-06 21:30:31 +0800

    Create mines.cpp

---
+++ mines.cpp
@@ -0,0 +1,28 @@
+#include "mines.h"
+#include <stdlib.h> 
+
+void Mines_implementation(int mines[][2], char minesBoard[][16], int side, int num_mines)
+{
+	
+	bool check[side*side];
+
+
+	for (int i=0; i<num_mines; )
+	{
+		int random = rand() % (side*side);
+		int x = random / side;
+		int y = random % side;
+
+		if (check[random] == false)
+		{
+			mines[i][0]= x;
+			mines[i][1] = y;
+
+			minesBoard[mines[i][0]][mines[i][1]] = '*';
+			check[random] = true;
+			i++;
+		}
+	}
+
+	return;
+}
